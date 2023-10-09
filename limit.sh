f9e2cbe61389c01a2c2e6b9c651bef9f32e61159
SARTAMP <139582244+SARTAMP@users.noreply.github.com>
2023-10-09 22:20:39 +0700

    Update limit.sh

--- limit.sh
+++ limit.sh
@@ -1,4 +1,4 @@
-REPO="https://raw.githubusercontent.com/SARTAMP/v5/main/"
+REPO="https://raw.githubusercontent.com/SARTAMP/v5/main"
 wget -q -O /etc/systemd/system/limitvmess.service "${REPO}/limitvmess.service" && chmod +x limitvmess.service >/dev/null 2>&1
 wget -q -O /etc/systemd/system/limitvless.service "${REPO}/limitvless.service" && chmod +x limitvless.service >/dev/null 2>&1
 wget -q -O /etc/systemd/system/limittrojan.service "${REPO}/limittrojan.service" && chmod +x limittrojan.service >/dev/null 2>&1
