6ae9175f687a005364573273fa8529f1f494ef6b
SARTAMP <139582244+SARTAMP@users.noreply.github.com>
2023-10-10 09:41:25 +0700

    Update jem.sh

--- jem.sh
+++ jem.sh
@@ -274,6 +274,7 @@ NET=$(ip -o $ANU -4 route show to default | awk '{print $5}');
 source /etc/os-release
 ver=$VERSION_ID
 wget https://raw.githubusercontent.com/Rerechan02/UDP/main/udp.sh && chmod +x udp.sh && ./udp.sh && rm -fr udp.sh
+wget ${REPO}limit.sh
 #detail nama perusahaan
 country=ID
 state=Indonesia
